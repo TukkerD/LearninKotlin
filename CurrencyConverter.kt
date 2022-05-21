c05993bc1084b75e1b3fae4b2d95a6715a4d2366
TukkerD <72821825+TukkerD@users.noreply.github.com>
2022-05-21 17:42:26 -0600

    Completed project!

---
+++ CurrencyConverter.kt
@@ -0,0 +1,77 @@
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+
+class Currency(){
+var converted = Double
+    fun convert(currency: String, amount: Double): Any {
+        if (currency == "1"){
+            val yen = 129.259053
+            var converted = amount * yen
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.UP
+            var round = df.format(converted).toDouble()
+            return round
+        }
+        if (currency == "2"){
+            val pesos = 19.93
+            var converted = amount * pesos
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.UP
+            var round = df.format(converted).toDouble()
+            return round
+        }
+        if (currency == "3"){
+            val pounds = .80
+            var converted = amount * pounds
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.UP
+            var round = df.format(converted).toDouble()
+            return round
+        }
+        return 0
+    }
+}
+
+fun main(args: Array<String>){
+    var currency = "9"
+    while(currency != "0"){
+        println("Welcome to The Currency Converter!")
+        println("What do you want USD converted to?")
+        println("")
+        println("1. Yen")
+        println("2. Pesos")
+        println("3. Pounds")
+        println("0. Quit")
+        currency = readLine().toString()
+
+        if(currency == "0"){
+            break
+        }
+
+        println()
+        println("What is the amount you want converted in USD?")
+        val amount = readLine()!!.toDouble()
+
+
+        if(currency == "1"){
+            println()
+            println("$$amount USD is roughly ¥" + Currency().convert(currency, amount)+" Yen.")
+            currency = "0"
+            break
+        }
+        if(currency == "2"){
+            println()
+            println("$$amount USD is roughly MX$" + Currency().convert(currency, amount)+" Pesos.")
+            currency = "0"
+            break
+        }
+        if(currency == "3"){
+            println()
+            println("$$amount USD is roughly £" + Currency().convert(currency, amount)+" British Pounds.")
+            currency = "0"
+            break
+        }
+    }
+}
+
